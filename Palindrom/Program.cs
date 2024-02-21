62da23f7d657aa546f1d425984848e23eb3c2809
Jakub Pacler <jakub.pacler@gmail.com>
2024-02-21 13:05:47 +0100

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Program sprawdza czy wprowadzone słowo jest palindromem");
+Console.Write("Wprowadź słowo do sprawdzenia: ");
+string word = Console.ReadLine();
+
+bool palindrome = true;
+
+
+int length = word.Length;
+for (int i = 0; i < length / 2; i++)
+{
+    if (word[i] != word[length - i - 1])
+    {
+        palindrome = false;
+    }
+}
+
+if (palindrome)
+{
+    Console.WriteLine("Słowo '" + word + "' jest palindromem");
+}
+else
+{
+    Console.WriteLine("Słowo '" + word + "' nie jest palindromem");
+}
